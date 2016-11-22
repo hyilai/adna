17f323d33172dac76cc86a20a0bb1718a3354e13
hyilai <jessl93@hotmail.com>
2016-11-22 01:10:07 -0600

    Started on MPI functions

--- readFastq.cpp
+++ readFastq.cpp
@@ -17,68 +17,31 @@
 #include <iomanip>
 #include <iostream>
 #include <fstream>
+#include <mpi.h>
 #include "SmithWaterman.hpp"
 
 using namespace std;
 
 class SmithWaterman;
 
-int main (int argc, char** argv) {
+// containers
+vector<vector<string> > file_line;
+vector< vector<string> > trimmed_seq;
+vector<string> adapters;
 
-	if (argc != 4) {
-		cerr << "Arguments: <fastq_file> <adapter_file> <output_file>" << endl;
-		exit(1); 
-	}
+void parse_file (int start, int end) {
 
-	ifstream file(argv[1]);
-	ifstream adpt(argv[2]);
+	// trim fastq file
+	// read 4 lines in a roll (4 lines = 1 set of data)
+	for (int i = start; i < end; i++) {
 
-	vector< vector<string> > trimmed_seq;
-	vector<string> adapters;
-
-	// get the list of adapters
-	string ad;
-	while (1) {
-		if (adpt.peek() == EOF) {
-			break;
-		}
-
-		//discard first line
-		getline(adpt, ad);
-
-		//get adapter sequence in the second line
-		getline(adpt, ad);
-		adapters.push_back(ad);
-	}
-
-
-	ofstream out(argv[3]);
-
-	int j = 0;
-
-	// read fastq file
-	while(1) {
-		if (file.peek() == EOF) {
-			break;
-		}
-
-		cout << "Reading line " << j++ << "..." << endl;
+		cout << "Parsing line " << i << "..." << endl;
 		string line1, line2, line3, line4, trimmed, quality;
 
-		// first line of read
-		// information of the read
-		getline(file, line1);
-
-		// second line of read
-		// sequence read
-		getline(file, line2);
-
-		// third line of read
-		getline(file, line3);
-
-		// fourth line of read 
-		// quality control
-		getline(file, line4);
+		line1 = file_line[i][0];
+		line2 = file_line[i][1];
+		line3 = file_line[i][2];
+		line4 = file_line[i][3];
 
 		for (int i = 0; i < adapters.size(); i++) {
 
@@ -97,10 +60,13 @@ int main (int argc, char** argv) {
 		}
 
 		// output new data
-		out << line1 << endl
-			<< trimmed << endl
-			<< line3 << endl
-			<< quality << endl;
+		//out << line1 << endl
+		//	<< trimmed << endl
+		//	<< line3 << endl
+		//	<< quality << endl;
+		// WIP; put them into the vector for now
+		file_line[i][1] = trimmed;
+		file_line[i][3] = quality;
 	}
 
 	file.close();
@@ -108,3 +74,67 @@ int main (int argc, char** argv) {
 
 	return 0;
 }
+
+
+int main (int argc, char** argv) {
+
+	if (argc != 4) {
+		cerr << "Arguments: <fastq_file> <adapter_file> <output_file>" << endl;
+		exit(1); 
+	}
+
+	ifstream file(argv[1]);
+	ifstream adpt(argv[2]);
+
+	// get the list of adapters
+	string ad;
+	while (1) {
+		if (adpt.peek() == EOF) {
+			break;
+		}
+
+		//discard first line
+		getline(adpt, ad);
+
+		//get adapter sequence in the second line
+		getline(adpt, ad);
+		adapters.push_back(ad);
+	}
+
+	// get all file lines
+	string line;
+	while (getline(file,line)) {
+		vector<string> line_group;
+		line_group.push_back(line);	// first line; information of the read
+		getline(file, line);
+		line_group.push_back(line);	// second line; sequence read
+		getline(file, line);
+		line_group.push_back(line);	// third line
+		getline(file, line);
+		line_group.push_back(line);	// fourth line; quality control
+		file_lines.push_back(line_group);
+	}
+
+	ofstream out(argv[3]);
+
+	// set up MPI stuff
+	int comm_sz;
+	int my_rank;
+
+	MPI_Init(NULL,NULL);
+	MPI_comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	if (my_rank != 0) {
+
+	} else {
+
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
+
+
+
