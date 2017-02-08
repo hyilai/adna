8030030a069deb32fd88a9513949704f8eb1330a
mchomestar <alleng.mcd@gmail.com>
2017-02-07 23:13:51 -0600

    Add files via upload

--- test_matchingMPI.cpp
+++ test_matchingMPI.cpp
@@ -23,7 +23,7 @@ int main (int argc, char** argv) {
 	srand(time(NULL));
 	
 	int comm_sz;    /* number of processes  */
-    	int my_rank;    /* process rank         */
+	int my_rank;    /* process rank         */
 	
 	
 	if (argc != 3) {
@@ -56,66 +56,95 @@ int main (int argc, char** argv) {
 	
 	//Get the number of processes in MPI_COMM_WORLD, and put 
 	//it in the 'comm_sz" variable
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
+   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	int counter = 0;
 	
 	while(getline(in1, info1) && getline(in2, info2)) {
 
-		//
-		string sequence1, na1, quality1;
-		string sequence2, na2, quality2;
-		
-		// read lines from in1
-		getline(in1, sequence1);
-		getline(in1, na1);
-		getline(in1, quality1);
 
-		// read lines from in2
-		getline(in2, sequence2);
-		getline(in2, na2);
-		getline(in2, quality2);
+		printf("%d %d \n", counter % comm_sz, my_rank );
 
-		cout << "Reading line " << j+1 << "..." << endl;
+		if((counter % comm_sz) ==  my_rank) {
 
-		string matched = "";
-		int highest_score = 0;
+			//
+			string sequence1, na1, quality1;
+			string sequence2, na2, quality2;
+			
+			// read lines from in1
+			getline(in1, sequence1);
+			getline(in1, na1);
+			getline(in1, quality1);
 
-		// start clock for sw algorithm
-		clock_t start_time = clock();
+			// read lines from in2
+			getline(in2, sequence2);
+			getline(in2, na2);
+			getline(in2, quality2);
 
-		// check one direction
-		SmithWaterman* sw = new SmithWaterman(sequence1, sequence2, quality1, quality2, match_score);
-		int curr_high_score = sw->get_highest_score();
-		if (highest_score < curr_high_score) {
-			if (sw->match_reads()) {
-				matched = sw->get_matched_string();
+			cout << "Reading line " << j+1 << "..." << endl;
+
+			string matched = "";
+			int highest_score = 0;
+
+			// start clock for sw algorithm
+			clock_t start_time = clock();
+
+			// check one direction
+			SmithWaterman* sw = new SmithWaterman(sequence1, sequence2, quality1, quality2, match_score);
+			int curr_high_score = sw->get_highest_score();
+			if (highest_score < curr_high_score) {
+				if (sw->match_reads()) {
+					matched = sw->get_matched_string();
+				}
 			}
-		}
-		delete sw;
+			delete sw;
 
-		// check the other direction
-		sw = new SmithWaterman(sequence2, sequence1, quality2, quality1, match_score);
-		curr_high_score = sw->get_highest_score();
-		if (highest_score < curr_high_score) {
-			if (sw->match_reads()) {
-				matched = sw->get_matched_string();
+			// check the other direction
+			sw = new SmithWaterman(sequence2, sequence1, quality2, quality1, match_score);
+			curr_high_score = sw->get_highest_score();
+			if (highest_score < curr_high_score) {
+				if (sw->match_reads()) {
+					matched = sw->get_matched_string();
+				}
 			}
+			delete sw;
+
+
+			// end clock for sw algorithm
+			clock_t end_time = clock();
+
+			// for calculations
+			total_elapsed_time.push_back(double(end_time - start_time) / CLOCKS_PER_SEC);
+
+			// output file
+			if (matched.length() > 0) {
+				out << matched << endl;
+			}
+
+			//increment loop
+			
+
 		}
-		delete sw;
+		else {
 
+			//
+			string sequence1, na1, quality1;
+			string sequence2, na2, quality2;
 
-		// end clock for sw algorithm
-		clock_t end_time = clock();
+			// read lines from in1
+			getline(in1, sequence1);
+			getline(in1, na1);
+			getline(in1, quality1);
 
-		// for calculations
-		total_elapsed_time.push_back(double(end_time - start_time) / CLOCKS_PER_SEC);
-
-		// output file
-		if (matched.length() > 0) {
-			out << matched << endl;
+			// read lines from in2
+			getline(in2, sequence2);
+			getline(in2, na2);
+			getline(in2, quality2);
 		}
 
-		//increment loop
 		j++;
+		counter++;
 	}
 	
 	/*Inform MPI that this process has finished*/
