113fd3f7916d81275c0a8e9a4f92192cf782062b
mchomestar <alleng.mcd@gmail.com>
2017-03-26 23:27:17 -0500

    Add files via upload

--- MPI_readFastq.cpp
+++ MPI_readFastq.cpp
@@ -20,6 +20,10 @@
 #include <unordered_map>
 #include <mpi.h>
 #include "SmithWaterman.hpp"
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include "sys/times.h"
+#include "sys/vtimes.h"
 
 using namespace std;
 
@@ -63,6 +67,13 @@ const int CONCAT_SEQ_TAG = 17;
 const int CONCAT_QUAL_TAG = 18;
 const int CONCAT_END_TAG = 20;
 
+
+
+
+struct sysinfo memInfo;
+static unsigned long long lastTotalUser, lastTotalUserLow, lastTotalSys, lastTotalIdle;
+static clock_t lastCPU, lastSysCPU, lastUserCPU;
+static int numProcessors;
 
 // struct for holding info for a read
 struct DNAread {
@@ -1159,12 +1170,147 @@ void print_argument_directions(char* program) {
 }
 
 
+int parseLine(char* line){
+    // This assumes that a digit will be found and the line ends in " Kb".
+    int i = strlen(line);
+    const char* p = line;
+    while (*p <'0' || *p > '9') p++;
+    line[i-3] = '\0';
+    i = atoi(p);
+    return i;
+}
+
+int getValue(){ //Note: this value is in KB!
+    FILE* file = fopen("/proc/self/status", "r");
+    int result = -1;
+    char line[128];
+
+    while (fgets(line, 128, file) != NULL){
+        if (strncmp(line, "VmRSS:", 6) == 0){
+            result = parseLine(line);
+            break;
+        }
+    }
+    fclose(file);
+    return result;
+}
+
+
+void initCPUCurrent(){
+    FILE* file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &lastTotalUser, &lastTotalUserLow,
+        &lastTotalSys, &lastTotalIdle);
+    fclose(file);
+}
+
+double CPUCurrent(){
+    double percent;
+    FILE* file;
+    unsigned long long totalUser, totalUserLow, totalSys, totalIdle, total;
+
+    file = fopen("/proc/stat", "r");
+    fscanf(file, "cpu %llu %llu %llu %llu", &totalUser, &totalUserLow,
+        &totalSys, &totalIdle);
+    fclose(file);
+
+    if (totalUser < lastTotalUser || totalUserLow < lastTotalUserLow ||
+        totalSys < lastTotalSys || totalIdle < lastTotalIdle){
+        //Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else{
+        total = (totalUser - lastTotalUser) + (totalUserLow - lastTotalUserLow) +
+            (totalSys - lastTotalSys);
+        percent = total;
+        total += (totalIdle - lastTotalIdle);
+        percent /= total;
+        percent *= 100;
+    }
+
+    lastTotalUser = totalUser;
+    lastTotalUserLow = totalUserLow;
+    lastTotalSys = totalSys;
+    lastTotalIdle = totalIdle;
+
+    cout << percent << endl;
+
+    return percent;
+}
+
+
+void initCPUCurrentProcess(){
+    FILE* file;
+    struct tms timeSample;
+    char line[128];
+
+    lastCPU = times(&timeSample);
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+
+    file = fopen("/proc/cpuinfo", "r");
+    numProcessors = 0;
+    while(fgets(line, 128, file) != NULL){
+        if (strncmp(line, "processor", 9) == 0) numProcessors++;
+    }
+    fclose(file);
+}
+
+double	CPUCurrentProcess(){
+    struct tms timeSample;
+    clock_t now;
+    double percent;
+
+    now = times(&timeSample);
+    if (now <= lastCPU || timeSample.tms_stime < lastSysCPU ||
+        timeSample.tms_utime < lastUserCPU){
+        //Overflow detection. Just skip this value.
+        percent = -1.0;
+    }
+    else{
+        percent = (timeSample.tms_stime - lastSysCPU) +
+            (timeSample.tms_utime - lastUserCPU);
+        percent /= (now - lastCPU);
+        percent /= numProcessors;
+        percent *= 100;
+    }
+    lastCPU = now;
+    lastSysCPU = timeSample.tms_stime;
+    lastUserCPU = timeSample.tms_utime;
+
+    cout << percent << endl;
+
+    return percent;
+}
+
+void GetMemoryUsage() {
+	sysinfo (&memInfo);
+	long long totalVirtualMem = memInfo.totalram;
+	//Add other values in next statement to avoid int overflow on right hand side...
+	totalVirtualMem += memInfo.totalswap;
+	totalVirtualMem *= memInfo.mem_unit;
+	long long totalPhysMem = memInfo.totalram;
+	//Multiply in next statement to avoid int overflow on right hand side...
+	totalPhysMem *= memInfo.mem_unit;
+	long long physMemUsed = memInfo.totalram - memInfo.freeram;
+	//Multiply in next statement to avoid int overflow on right hand side...
+	physMemUsed *= memInfo.mem_unit;
+
+	cout << "Total physical memory: " << totalPhysMem << endl;
+	cout << "Physical memory currently used" << physMemUsed << endl;
+	cout << "CPU currently csed" << initCPUCurrent(); << endl;
+	cout << "CPU currently used by current process:" << initCPUCurrentProcess(); << endl;	
+}
+
+
 int main (int argc, char** argv) {
 
 	if (argc < 4) {
 		print_argument_directions(argv[0]);
 		exit(1); 
 	}
+
+	initCPUCurrent();
+	initCPUCurrentProcess();
 
 	bool using_adapter_file = false;
 	bool use_MPI = false;
